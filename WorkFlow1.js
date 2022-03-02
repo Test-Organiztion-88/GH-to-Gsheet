1ec63dc11c947256f76a143143d53794a4169e07
EJ Labaria <43556136+EJLabaria@users.noreply.github.com>
2022-03-01 16:11:51 -0800

    Update WorkFlow1.js

--- WorkFlow1.js
+++ WorkFlow1.js
@@ -1,7 +1,11 @@
-function Test(){
-};
+//Forked from main GH-to-Gsheet//
+//eliminating all functions and making into one//
+//Date 3.1.22//
 
-function Front_fontSize_ColumnWrap() {
+//Next eliminate duplicate 'var spreadsheet = ...'//
+
+function Test1(){
+
   var spreadsheet = SpreadsheetApp.getActive();
   spreadsheet.getRange('A1').activate();
   var currentCell = spreadsheet.getCurrentCell();
@@ -10,17 +14,12 @@ function Front_fontSize_ColumnWrap() {
   spreadsheet.getActiveRangeList().setFontFamily('Arial')
   .setFontSize(11)
   .setWrapStrategy(SpreadsheetApp.WrapStrategy.CLIP);
-};
 
-function sort_LinkedInLinks() {
   var spreadsheet = SpreadsheetApp.getActive();
   spreadsheet.getRange('M:M').activate();
   spreadsheet.getActiveSheet().moveColumns(spreadsheet.getRange('M:M'), 5);
   spreadsheet.getRange('R:R').activate();
-  spreadsheet.getActiveSheet().moveColumns(spreadsheet.getRange('R:R'), 6);
-};
 
-function add_extraColumns() {
   var spreadsheet = SpreadsheetApp.getActive();
   spreadsheet.getRange('G:G').activate();
   spreadsheet.getActiveSheet().insertColumnsBefore(spreadsheet.getActiveRange().getColumn(), 1);
@@ -33,17 +32,13 @@ function add_extraColumns() {
   spreadsheet.getActiveRange().offset(0, 0, spreadsheet.getActiveRange().getNumRows(), 1).activate();
   spreadsheet.getActiveSheet().insertColumnsBefore(spreadsheet.getActiveRange().getColumn(), 1);
   spreadsheet.getActiveRange().offset(0, 0, spreadsheet.getActiveRange().getNumRows(), 1).activate();
-};
 
-function Header_Bold_Center() {
   var spreadsheet = SpreadsheetApp.getActive();
   spreadsheet.getRange('1:1').activate();
   spreadsheet.getActiveSheet().setFrozenRows(1);
   spreadsheet.getActiveRangeList().setFontWeight('bold')
   .setHorizontalAlignment('center');
-};
 
-function Add_Status_Add_Notes() {
   var spreadsheet = SpreadsheetApp.getActive();
   spreadsheet.getRange('G1').activate();
   spreadsheet.getCurrentCell().setValue('Status');
@@ -54,9 +49,7 @@ function Add_Status_Add_Notes() {
   spreadsheet.getRange('G1:H1').activate();
   spreadsheet.getActiveRangeList().setHorizontalAlignment('center')
   .setFontWeight('bold');
-};
 
-function Add_colorHeader() {
   var spreadsheet = SpreadsheetApp.getActive();
   spreadsheet.getRange('F1').activate();
   var currentCell = spreadsheet.getCurrentCell();
@@ -82,17 +75,13 @@ function Add_colorHeader() {
   spreadsheet.getRange('H970').activate();
   spreadsheet.getCurrentCell().getNextDataCell(SpreadsheetApp.Direction.UP).activate();
   spreadsheet.getRange('H2').activate();
-};
 
-function Social_Link_Sort() {
   var spreadsheet = SpreadsheetApp.getActive();
   var sheet = spreadsheet.getActiveSheet();
   sheet.getRange(1, 1, sheet.getMaxRows(), sheet.getMaxColumns()).activate();
   spreadsheet.getActiveRange().offset(1, 0, spreadsheet.getActiveRange().getNumRows() - 1).sort({column: 5, ascending: true});
   spreadsheet.getRange('F2').activate();
-};
 
-function Resize_Notes() {
   var spreadsheet = SpreadsheetApp.getActive();
   spreadsheet.getRange('I9').activate();
   spreadsheet.getActiveSheet().setColumnWidth(8, 321);
